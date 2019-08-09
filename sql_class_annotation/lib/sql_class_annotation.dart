6c34ec57d4f83a7203159d57e957ee703fd2280d
Cat <a599231042@163.com>
2019-08-09 09:53:40 +0800

    init

---
+++ sql_class_annotation/lib/sql_class_annotation.dart
@@ -0,0 +1,9 @@
+library sql_data_annotation;
+
+export 'src/allowed_keys_helpers.dart';
+export 'src/checked_helpers.dart';
+export 'src/class_converter.dart';
+export 'src/class_key.dart';
+export 'src/class_literal.dart';
+export 'src/class_serializable.dart';
+export 'src/class_value.dart';
---
+++ sql_class_serializable/lib/sql_class_serializable.dart
@@ -0,0 +1,4 @@
+library sql_data_serializable;
+
+export 'src/json_literal_generator.dart' show JsonLiteralGenerator;
+export 'src/json_serializable_generator.dart' show JsonSerializableGenerator;
